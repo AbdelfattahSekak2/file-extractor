c8a0e56a92cbfd57705ebe8e6afca60d1c3d1d99
elinoi <abdlfattah.sekak@centrale-casablanca.ma>
2019-08-10 12:35:24 +0200

    Configure project environment

---
+++ src/store.ts
@@ -0,0 +1,23 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { all } from "redux-saga/effects";
+
+const rootReducer = combineReducers({});
+
+function* rootSaga() {
+  yield all([]);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      sagaMiddleware,
+    ),
+  ),
+);
+
+sagaMiddleware.run(rootSaga);
